b7a05c2aa4cf2f481c89b9cb94dc9a9ef0e35356
leejaejae <leesengjie@naver.com>
2025-01-16 19:58:30 +0900

    feat: emailJs 사용해서 contact 구현

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import TopButton from "./components/TopBotton";
 import AboutDetail from "./details/AboutDetail";
 import ArchivingDetail from "./details/ArchivingDetail";
 import SkillDetail from "./details/SkillDetail";
+import ContactDetail from "./details/ContactDetail";
 
 function App() {
   const HomeRef = useRef(null);
   const ArchivingRef = useRef(null);
   const SkillsRef = useRef(null);
+  const ContactRef = useRef(null);
 
   const moveToHomeHandler = () => {
     HomeRef.current.scrollIntoView({ behavior: "smooth" });
@@ -24,13 +26,22 @@ function App() {
   const moveToSkillsHandler = () => {
     SkillsRef.current.scrollIntoView({ behavior: "smooth" });
   };
+  const moveToContactHandler = () => {
+    ContactRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <div className="App">
-      <Header moveToHome={moveToHomeHandler} moveToArc={moveToArcHandler} />
+      <Header
+        moveToHome={moveToHomeHandler}
+        moveToArc={moveToArcHandler}
+        moveToSkill={moveToSkillsHandler}
+        moveToContact={moveToContactHandler}
+      />
       <AboutDetail ref={HomeRef} moveToArc={moveToHomeHandler} />
       <ArchivingDetail ref={ArchivingRef} />
       <SkillDetail ref={SkillsRef} />
+      <ContactDetail ref={ContactRef} />
       <TopButton />
       <div className="footer">
         <Footer />
