3ba249ab185e6cd1cab300edbee08d5c70855e64
leejaejae <leesengjie@naver.com>
2025-01-16 20:04:15 +0900

    feat: scroll 애니메이션 추가

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   const ArchivingRef = useRef(null);
   const SkillsRef = useRef(null);
   const ContactRef = useRef(null);
+  const [visibleSections, setVisibleSections] = useState([]);
 
   const moveToHomeHandler = () => {
     HomeRef.current.scrollIntoView({ behavior: "smooth" });
@@ -30,6 +31,34 @@ function App() {
     ContactRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = [
+        { ref: ArchivingRef, id: "archiving" },
+        { ref: SkillsRef, id: "skills" },
+        { ref: ContactRef, id: "contact" },
+      ];
+
+      const visibleSections = sections
+        .filter(({ ref }) => {
+          const rect = ref.current.getBoundingClientRect();
+          return (
+            rect.top <= window.innerHeight / 2 &&
+            rect.bottom >= window.innerHeight / 4
+          );
+        })
+        .map(({ id }) => id);
+
+      setVisibleSections(visibleSections);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Header
@@ -39,9 +68,31 @@ function App() {
         moveToContact={moveToContactHandler}
       />
       <AboutDetail ref={HomeRef} moveToArc={moveToHomeHandler} />
-      <ArchivingDetail ref={ArchivingRef} />
+      {/* <ArchivingDetail ref={ArchivingRef} />
       <SkillDetail ref={SkillsRef} />
-      <ContactDetail ref={ContactRef} />
+      <ContactDetail ref={ContactRef} /> */}
+      <div
+        ref={ArchivingRef}
+        className={`section ${
+          visibleSections.includes("archiving") && "visible"
+        }`}
+      >
+        <ArchivingDetail />
+      </div>
+      <div
+        ref={SkillsRef}
+        className={`section ${visibleSections.includes("skills") && "visible"}`}
+      >
+        <SkillDetail />
+      </div>
+      <div
+        ref={ContactRef}
+        className={`section ${
+          visibleSections.includes("contact") && "visible"
+        }`}
+      >
+        <ContactDetail />
+      </div>
       <TopButton />
       <div className="footer">
         <Footer />
--- src/details/AboutDetail.jsx
+++ src/details/AboutDetail.jsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from "react";
 import "../style/details/AboutDetail.css";
 import myphoto from "../assets/profile.png";
 
-const aboutDetail = forwardRef((pros, ref) => {
+const aboutDetail = forwardRef((props, ref) => {
   return (
     <div className="home" ref={ref}>
       <div className="home-myPhoto">
@@ -11,6 +11,7 @@ const aboutDetail = forwardRef((pros, ref) => {
       <div className="home-about">
         <h1 className="home-title">안녕하세요.</h1>
         <div className="home-text">
+          <p></p>
           <p>
             <span className="highlight">이승재</span> 입니다.
           </p>
